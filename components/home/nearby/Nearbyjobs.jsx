4febbd0487d6a3bbfec4aa73d6645da961c6dd3d
Kshitij Gupta <kshitij.gupta2301@gmail.com>
2024-03-04 02:13:37 +0530

    Completed job details page

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,5 +1,6 @@
+import React from "react";
 import { useRouter } from "expo-router";
-import { View,Text,TouchableOpacity,ActivityIndicator} from "react-native";
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
 
 import styles from "./nearbyjobs.style";
 import { COLORS } from "../../../constants";
@@ -9,7 +10,7 @@ import useFetch from "../../../hook/useFetch";
 const Nearbyjobs = () => {
   const router = useRouter();
   const { data, isLoading, error } = useFetch("search", {
-    query: "React developer",
+    query: "React Native developer",
     num_pages: "1",
   });
 
@@ -31,7 +32,7 @@ const Nearbyjobs = () => {
           data?.map((job) => (
             <NearbyJobCard
               job={job}
-              key={`nearby-job-${job?.job_id}`}
+              key={`nearby-job-${job.job_id}`}
               handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
             />
           ))
