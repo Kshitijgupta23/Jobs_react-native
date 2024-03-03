0214a312c29ec8d988c634c80e73d635ae711bae
Kshitij Gupta <kshitij.gupta2301@gmail.com>
2024-03-03 21:51:09 +0530

    Completed UI of home screen

--- app/_layout.js
+++ app/_layout.js
@@ -1,9 +1,42 @@
+import React, { useCallback, useEffect, useState } from 'react';
 import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
-const Layout = () =>{
-    return(
-        <Stack />
-    )
-}
+const Layout = () => {
+    const [fontsLoaded, error] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    });
+
+    const [isReady, setIsReady] = useState(false);
+
+    useEffect(() => {
+        const loadResourcesAsync = async () => {
+            try {
+                await SplashScreen.preventAutoHideAsync();
+                if (fontsLoaded) {
+                    await SplashScreen.hideAsync();
+                    setIsReady(true);
+                }
+            } catch (e) {
+                console.warn(e);
+            }
+        };
+        loadResourcesAsync();
+    }, [fontsLoaded]);
+
+    if (error) {
+        console.error('Failed to load fonts:', error);
+    }
+
+    if (!isReady) {
+        // You might want to render a loading indicator here
+        return null;
+    }
+
+    return <Stack />;
+};
 
 export default Layout;
